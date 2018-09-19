83bb261555159a151161f48e60ab45b618b341de
TharakaMadushanki <tharakamadushanki@gmail.com>
2018-09-19 18:58:08 +0530

    added disply updated page

---
+++ CSRF_Prevention_Synchronizer_Token_Pattern/results.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Cross Site Request Forgery Protection</title>
+		<link rel="stylesheet"  href="./public/styles/bootstrap.min.css">
+		<link rel="stylesheet"  href="./public/styles/style.css">
+	</head>
+	<body>
+		<form name="results">
+			<div class="results">
+				<h3>Updated Successfully</h3>			
+				<?php
+
+					require_once 'token.php';
+
+					$csrfTokenValue = $_POST["token"];
+
+					if(isset($_POST['updatepost'])){
+						if(token::checkToken($csrfTokenValue,$_COOKIE['sessionId'])){
+							echo "<div class='alert alert-secondary'>".$_POST['updatepost']."</div>";		
+						}	
+						else{
+							echo "Error In Updating Status";
+						}
+					} 
+				?>
+			</div>					
+		</form>						
+		<div class="footer">
+			<p>Cross Site Request Forgery Protection  |  Synchronizer Token Pattern  |  Tharaka Liyanage  |  IT13015886</p>
+		</div>
+	</body> 
+</html>
